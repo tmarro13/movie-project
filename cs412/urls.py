40fd7d29fc30b74e8b1be2a429c7562aa922d377
Tim Marro <tmarro@bu.edu>
2024-12-09 01:38:30 -0500

    static files

--- cs412/urls.py
+++ cs412/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('project/', include('project.urls')),  # Include URLs for the project app 
-] 
+] + static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Add static and media URL handling
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
